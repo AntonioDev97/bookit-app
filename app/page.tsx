8b81186c7b5e275b03b64cd4c9b8439ad4264054
Antonio Olvera <antonio.olvera@ibm.com>
2024-04-21 21:36:43 -0600

    feat: final changes 100% project complete

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,18 @@ export default async function HomePage({ searchParams }: { searchParams: string 
 
   const { API_URL } = process.env;
   const URLParams = new URLSearchParams(searchParams)?.toString();
-  const data = await (await fetch(`${API_URL}/api/rooms?${URLParams}`, {
-    next: { tags: ['RoomDetails'] }
-  })).json();
+  let data = { message: 'error' } as any;
+
+  try {
+    data = await (await fetch(`${API_URL}/api/rooms?${URLParams}`, {
+      next: { tags: ['RoomDetails'] }
+    })).json();
+  } catch (error) {
+    console.error(error);
+  }
 
   if (data?.message) return <Error error={data} />;
   return (
-    <Home data={data.data} />
+    <Home data={data?.data} />
   )
 }
